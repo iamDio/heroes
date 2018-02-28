a752746f3050bd12849ed0f0f78b2dca74580c42
iamDio <claudio.villani00@gmail.com>
2018-02-28 17:32:29 -0500

    added mock-heroes data to populate a list on our page

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice',superPower:'induce serotonin release' },
+  { id: 12, name: 'Narco' ,superPower:'become a drug kingpin'},
+  { id: 13, name: 'Bombasto',superPower:'summon explosions from the sky' },
+  { id: 14, name: 'Celeritas' ,superPower:'run at the speed of light'},
+  { id: 15, name: 'Magneta' ,superPower:'attracts all metals to hands'},
+  { id: 16, name: 'RubberMan' ,superPower:'bounce off any surface from most heights'},
+  { id: 17, name: 'Dynama',superPower:'travel between dymensions' },
+  { id: 18, name: 'Dr IQ',superPower:'psychic' },
+  { id: 19, name: 'Magma',superPower:'rain of fire' },
+  { id: 20, name: 'Tornado',superPower:'create a tornado with bare hands' }
+];
